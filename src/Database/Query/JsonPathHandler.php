3af9a6169ee7be707712a00189857dfe8bc51b9f
rajeshn95 <52703970+rajeshn95@users.noreply.github.com>
2025-02-03 09:54:18 +0000

    Fix styling

--- src/Database/Query/JsonPathHandler.php
+++ src/Database/Query/JsonPathHandler.php
@@ -13,6 +13,7 @@ class JsonPathHandler
     public function handle(string $path, ?string $alias = null): Expression
     {
         $expression = $this->buildJsonPath($path);
+
         // Aliase for the json column
         // if ($alias !== null) {
         //     $expression .= ' as '.$alias;
